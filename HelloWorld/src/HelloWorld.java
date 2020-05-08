62a91ac22e7c2991ee9458c0b6f232acf0efa3c8
dsafawef <dsafawef@adkfne.com>
2020-05-08 11:05:24 +0900

    2020/05/08 수행평가4 실습

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+ * 날짜 : 2020/05/08
+ * 이름 : 신동호
+ * 내용 : HelloWorld 수행평가 실행
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+}
